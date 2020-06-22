0d73cdd43bd647c28dde7b9339fc085fd723500f
Sjors <sjorsv11@gmail.com>
2020-06-22 21:47:19 +0200

    DialogueSystem
    DialogueSystem added & working

---
+++ Assets/_Core/Scripts/DialogueTrigger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+
+    public Dialogue dialogue;
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.gameObject.tag == "npc1")
+    //    {
+    //        TriggerDialogue(); // press F to pickup
+
+    //    }
+    //}
+
+    DialogueManager dialogueManager;
+
+    private void Start()
+    {
+        dialogueManager = DialogueManager.Instance;
+    }
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (dialogueManager.startedDialogue)
+                dialogueManager.DisplayNextSentence();
+            else
+                dialogueManager.StartDialogue(dialogue);
+
+
+        }
+    }
+
+}
